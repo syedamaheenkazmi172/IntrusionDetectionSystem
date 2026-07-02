b0596a9b5b09bfb9dbd20e11c2be52ef688e0468
Usman-Azhar <usman.azhar7100@gmail.com>
2026-07-02 15:12:50 -0400

    Day 6 ICMP FLood Detection

--- ids.py
+++ ids.py
@@ -4,8 +4,11 @@ from alert import alert
 
 THRESHOLD = 20
 WINDOW = 5
-
 syn_tracker = defaultdict(list)
+
+ICMP_THRESHOLD = 50
+ICMP_WINDOW = 1
+icmp_tracker = defaultdict(list)
 
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 
@@ -35,3 +38,21 @@ while True:
 				alert('PORT_SCAN', src_ip,
 					f'{len(syn_tracker[src_ip])} SYNs in {WINDOW}s',
 					severity = 2)
+
+	elif iph[6] == 1:
+		icmp_header = raw_data[34:38]
+		icmph = struct.unpack('!BBH', icmp_header)
+		icmp_type = icmph[0]
+		src_ip = socket.inet_ntoa(iph[8])
+		
+		if icmp_type == 8:
+			now = time.time()
+			icmp_tracker[src_ip].append(now)
+			icmp_tracker[src_ip] = [
+				t for t in icmp_tracker[src_ip]
+				if now - t < ICMP_WINDOW
+			]
+			if len(icmp_tracker[src_ip]) > ICMP_THRESHOLD:
+				alert('ICMP_FLOOD', src_ip,
+					f'{len(icmp_tracker[src_ip])} pings in {ICMP_WINDOW}s',
+                                        severity=3)
